9600aa468e7c530184e29446435c7ab8c9bf8c86
kevnande <kevinfer26798@gmail.com>
2025-01-30 16:19:33 -0600

    Update PROYECTO_FINAL.py

--- PROYECTO_FINAL.py
+++ PROYECTO_FINAL.py
@@ -37,31 +37,28 @@ else:
     st.info("Selecciona el checkbox para mostrar la lista de filmes.")
 
 
+# Búsqueda por nombre
 search_name = st.sidebar.text_input("Buscar por nombre:")
-if st.sidebar.button("Buscar Nombre"):
-    if search_name:
-        filtered_df = df[df['name'].str.contains(search_name, case=False, na=False)]
-        st.write(f"Se encontraron {len(filtered_df)} filmes.")
-        st.dataframe(filtered_df)
-    else:
-        st.info("Escribe un nombre para buscar.")
+if search_name:
+    filtered_df = df[df['name'].str.contains(search_name, case=False, na=False)]
+    st.write(f"Se encontraron {len(filtered_df)} filmes.")
+    st.dataframe(filtered_df)
 
 
+# Filtrar por director
 if 'director' in df.columns:
     directors = df['director'].dropna().unique().tolist()
 else:
     directors = []  # Si no existe la columna, se asigna una lista vacía
 
 selected_director = st.sidebar.selectbox("Selecciona un director", directors)
-if st.sidebar.button("Filtrar por Director"):
-    if selected_director:
-        filtered_by_director = df[df['director'] == selected_director]
-        st.write(f"Se encontraron {len(filtered_by_director)} filmes dirigidos por {selected_director}.")
-        st.dataframe(filtered_by_director)
-    else:
-        st.info("Selecciona un director para filtrar.")
+if selected_director:
+    filtered_by_director = df[df['director'] == selected_director]
+    st.write(f"Se encontraron {len(filtered_by_director)} filmes dirigidos por {selected_director}.")
+    st.dataframe(filtered_by_director)
 
 
+# Insertar un nuevo filme
 with st.sidebar.form("insert_film_form"):
     st.header("Insertar un nuevo filme")
     new_name = st.text_input("Nombre:")
@@ -79,6 +76,7 @@ with st.sidebar.form("insert_film_form"):
                 new_film = {'name': new_name, 'genre': new_genre, 'director': new_director, 'company': new_company}
                 db.collection('netflix').add(new_film)
                 st.success(f"¡El filme '{new_name}' ha sido insertado exitosamente!")
-                df = load_data("netflix")  # Recargar datos
+                # Solo recargar datos si se ha insertado un nuevo filme
+                df = load_data("netflix")  
         else:
             st.error("Por favor completa todos los campos del formulario.")
