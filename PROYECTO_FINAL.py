7003a7d298e0f66e52481a8cc9cf441bc87a0082
kevnande <kevinfer26798@gmail.com>
2025-01-31 16:20:37 -0600

    Update PROYECTO_FINAL.py

--- PROYECTO_FINAL.py
+++ PROYECTO_FINAL.py
@@ -9,7 +9,7 @@ from google.oauth2 import service_account
 import json
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="movies-94cb0"
+db = firestore.Client(credentials=creds, project="movies-94cb0")
 
 
 
